489d74a9f92e6b2ed23218c3d311cabada0c4a6e
BRUCE <rooneyndiege@gmail.com>
2026-07-23 23:17:39 +0300

    Done fixing the kaleldo

---
+++ render-build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Install system dependencies for kaleido
+apt-get update && apt-get install -y \
+    libgconf-2-4 \
+    libatk1.0-0 \
+    libatk-bridge2.0-0 \
+    libcups2 \
+    libdrm2 \
+    libxkbcommon0 \
+    libxcomposite1 \
+    libxdamage1 \
+    libxfixes3 \
+    libxrandr2 \
+    libgbm1 \
+    libasound2
+
+# Then run your normal build
+pip install -r requirements.txt && python train_models.py
